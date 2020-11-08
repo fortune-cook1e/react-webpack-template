5f1a94a7adbe26aa5f58f1fc1f191b17782b46a0
fortune_cookie <767077147@qq.com>
2020-11-08 15:49:26 +0800

    chore(package.json): 添加husky 添加eslint和commit校验

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -4,57 +4,56 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const isDev = process.env.NODE_ENV === 'development'
 
 const resolve = (dir) => {
-  return path.join(__dirname,dir)
+  return path.join(__dirname, dir)
 }
-
 
 const jsReg = /\.(js|jsx)$/
 const cssReg = /\.(css)$/
 
 const config = {
-  entry:{
-    main:resolve('../src/main.jsx')
+  entry: {
+    main: resolve('../src/main.jsx')
   },
-  mode:isDev ? 'development' : 'production',
-  devtool:'source-map',
-  output:{
-    path:resolve('../dist'),
-    publicPath:isDev ? '/' :'./',
-    filename:'js/[name].[hash].js',
-    chunkFilename:'js/[name].[id].js'
+  mode: isDev ? 'development' : 'production',
+  devtool: 'source-map',
+  output: {
+    path: resolve('../dist'),
+    publicPath: isDev ? '/' : './',
+    filename: 'js/[name].[hash].js',
+    chunkFilename: 'js/[name].[id].js'
   },
-  resolve:{
-    extensions:['.js','.jsx'],
-    alias:{
-      '@':resolve('../src')
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': resolve('../src')
     }
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-        test:jsReg,
-        include:resolve('../src'),
-        exclude:/node_modules/,
-        use:[
+        test: jsReg,
+        include: resolve('../src'),
+        exclude: /node_modules/,
+        use: [
           {
-            loader:'babel-loader',
-            options:{
-              cacheDirectory:true
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
             }
           }
         ]
       },
       {
-        test:cssReg,
-        include:resolve('../src'),
-        exclude:/node_modules/,
-        use:['style-loader','css-loader']
+        test: cssReg,
+        include: resolve('../src'),
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
-  plugins:[
+  plugins: [
     new HtmlWebpackPlugin({
-      template:resolve('../public/index.html')
+      template: resolve('../public/index.html')
     })
   ]
 }
