968dece5fa3941ab7c139612ace44223ab315570
fortune cookie <767077147@qq.com>
2021-03-04 19:56:49 +0800

    build: 更新path

--- build/paths.js
+++ build/paths.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const { resolve } = require('path')
 
 module.exports = {
   // Source files
-  src: path.resolve(__dirname, '../src'),
+  src: resolve(__dirname, '../src'),
 
   // Production build files
-  build: path.resolve(__dirname, '../dist'),
+  build: resolve(__dirname, '../dist'),
 
   // Static files that get copied to build folder
-  public: path.resolve(__dirname, '../public'),
+  public: resolve(__dirname, '../public'),
 }
