337e4912804f14d269a77129311a2140829c7aae
fortune cookie <767077147@qq.com>
2021-03-11 23:20:08 +0800

    fix: 修复部署到gh-pages 白屏问题 - browserRouter 改为 hashRouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { renderRoutes } from 'react-router-config'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import routes from './routes'
 
 const RouterComponent = () => {
