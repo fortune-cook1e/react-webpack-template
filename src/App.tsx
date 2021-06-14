928b53c15cc6605e1a8258dfabd92ae756e207f9
fortune cookie <767077147@qq.com>
2021-06-14 16:05:36 +0800

    chore: 修改prettier eslint  配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const App: React.FC = () => {
-	return <div className='app'>this is app</div>
+  return (
+    <div className='app'>
+      <h1>this is app</h1>
+      <p>haha</p>
+    </div>
+  )
 }
 
 export default App
