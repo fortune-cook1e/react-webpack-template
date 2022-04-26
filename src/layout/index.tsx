a8d905d9db10a4835067401e703a51a684c3f04a
fortune cookie <767077147@qq.com>
2022-04-26 22:40:17 +0800

    ci(deploy): 修改ci文件 - 优化layout布局样式

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { Layout } from 'antd'
+const { Header, Footer, Sider, Content } = Layout
+
+import styles from './index.module.less'
 
 interface Props {
 	children: React.ReactNode
 }
 
-const Layout: React.FC<Props> = ({ children }) => {
+const DefaultLayout = ({ children }: Props): JSX.Element => {
 	return (
-		<div>
-			<h1>this is layout</h1>
-			{children}
-		</div>
+		<Layout className={styles.layout}>
+			<Header className={styles.header}>This is Header</Header>
+			<Layout className={styles.main}>
+				<Sider className={styles.sider}>Sider</Sider>
+				<Content className={styles.content}>{children}</Content>
+			</Layout>
+			<Footer className={styles.footer}>Footer</Footer>
+		</Layout>
 	)
 }
 
-export default Layout
+export default DefaultLayout
